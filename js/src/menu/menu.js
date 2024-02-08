577b965a994df6313e87476cac777e99e18700c9
Wagner Oliveira <74685635+WagProjects@users.noreply.github.com>
2024-02-08 02:51:04 -0300

    fix src

--- js/src/menu/menu.js
+++ js/src/menu/menu.js
@@ -2,7 +2,7 @@ export function criarElementoMenu() {
     const menuNavegacao = document.createElement('nav');
     menuNavegacao.classList.add('menu');
     menuNavegacao.innerHTML = `
-        <img src="../../../public/assets/images/Logo-01.png" alt="Logo" />
+        <img src="./public/assets/images/Logo-01.png" alt="Logo" />
         <ul class="menu__nav">
             <li onclick="active(this)"><a href="#">Home</a></li>
             <li onclick="active(this)"><a href="#" onclick="scrollElemento('.card__sobre')">Sobre</a></li>
