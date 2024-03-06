a091469a1c1a25ab9aaa9f9d3f72415a77160a76
Wagner Oliveira <74685635+WagProjects@users.noreply.github.com>
2024-03-06 15:42:16 -0300

    Delete js directory

--- js/src/main/main.js
+++
@@ -1,5 +0,0 @@
-export function criarContainerGeral() {
-    const containerGeral = document.createElement('section');
-    containerGeral.classList.add('container__geral');
-    return containerGeral;
-}
